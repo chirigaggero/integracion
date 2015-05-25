a87a90fa407eca8dac5e24fd69e2f7772d910bcd
jicelhay <juan.celhay@gmail.com>
2015-05-25 13:22:39 -0400

    bodega corregida

--- app/models/bodega.rb
+++ app/models/bodega.rb
@@ -5,7 +5,8 @@ class Bodega < ActiveRecord::Base
 
 
 	# Con un almacen id y sku se agregan a pedidos productos con sus ids
-	def self.obtener_prods?(almacen_id,cantidad,pedido)
+	def self.crear_prods(almacen_id,pedido,prods_necesitados)
+
 		params=["GET",almacen_id,pedido.sku]
 		security = Bodega.claveSha1(params)
 
@@ -19,32 +20,33 @@ class Bodega < ActiveRecord::Base
 
 		if !result.nil?
 			contador=0
-			pedidos=Pedido.first(Pedido.count-1)
+			##pedidos=Pedido.first(Pedido.count-1)
 			result.each do |item|
 				##antes de crear el producto, hay que verificar que su id no este en los pedidos ya registrados en la base de datos
-				aceptar=true
+		##		aceptar=true
 
 				#iterar sobre los pedidos, si el prod ya esta asignado, entonces cambia el bool aceptar a false.
-				pedidos.each do |pedido|
-					if !pedido.productos.where(prod_id: item["_id"]).empty?
-						aceptar=false
-					end
-				end
+				##pedidos.each do |pedido|
+				##	if !pedido.productos.where(prod_id: item["_id"]).empty?
+				#		aceptar=false
+			#		end
+		#		end
 				#si no está asignado, crea u producto con ese id y hace append sobre pedido.productos, aumenta el contador de productos
-				if aceptar
+			#	if aceptar
 					prod=Producto.create(prod_id: item["_id"])
 					pedido.productos<<prod
 					contador+=1
 					##si se llego a la cantidad requerida, retornamos true
-					if contador>=Integer(cantidad)
-						return true
+					if contador>=Integer(prods_necesitados)
+						break
 				end
-				end
+			end
+				return contador
 			end
 			##solo se llega aqui si no se alcanza la cantidad requerida, no deberia pasar pq ya hicimos el chequeo cuanto obtuvimos
 			##los productos 'disponibles'
 				return false
-		end
+		#end
 		end
 
 	##No estamos usando este metodo, pero no lo voy a borrar x si las moscas
@@ -86,6 +88,24 @@ class Bodega < ActiveRecord::Base
 	contador
 	end
 
+
+	##asigna id de productos a un pedido.
+	def armar_pedido?(pedido)
+		##conectarse a un almacen
+		prods_necesitados=pedido.cantidad
+
+		Bodega.first(4)[0..3].each do | almacen |
+
+		prods_necesitados-=crear_prods(almacen.id,pedido,prods_necesitados)
+
+		if prods_necesitados.equal?(0)
+			return true
+			end
+		end
+		return false
+	end
+
+
 	# Obtenemos la cantidad total dispoble de productos de un sku
 	def get_cantidad_total(url,header1,sku)
 		cantidad=0
@@ -119,9 +139,11 @@ class Bodega < ActiveRecord::Base
 		cant = pedido.cantidad
 		sku = pedido.sku
 		hay = false
-		contador = 0
-		elegido=[]
+		total = 0
+		usado=0
+		##elegido=[]
 
+		#se itera sobre las primeras 4 bodegas
 		Bodega.first(4)[0..3].each do | almacen |
 			params = ["GET", almacen.almacen_id]
 			security = claveSha1(params)
@@ -131,53 +153,71 @@ class Bodega < ActiveRecord::Base
 
 			##obtenemos la cantidad total del almacen
 			resultado = almacen.get_cantidad_total(url,header1,sku)
-
-			if resultado>0
-				##tenemos que ver cuanto realmente tenemos disponible
-				usado= get_cantidad_usada(almacen.id,pedido)
-				disponible=resultado-usado
-				##ver si necesitamos mas o menos de lo que tenemos disponible
-				##si lo disponible es mayor que 0
-				if disponible>0
-
-				if contador+disponible<cant
-					##si el pedido nos exige mas de lo que tenemos disponible
-					contador+=disponible
-					##hacemos append de el id del almacen y de la cantidad de prod. que usaremos en 'elegido'
-					elegido.append [almacen.almacen_id,disponible]
-					#si es menor, hacemos append de el id y la diferencia entre el contal y lo que llevamos
-				else
-					elegido.append [almacen.almacen_id,cant-contador]
-					##en teoria ya podemos satisfacer el pedido.
-					hay=true
-					break
-				end
-				end
-			end
+			total+=resultado
 		end
 
-		if hay
-			#hay que obtener los id's de los productos que vamos a usar.
-			#necesitamos llamar a metodo getstock con el sku y el almacen id
-			elegido.each do |eleg|
-				#La condicion es pq si es >200, entonces tenemos problemas con el display de productos.
-				# (tenemos limit=200)
-				if eleg[1]<200
-				#obtener prods retorna true si hay suficientes prods en bodega que no esten asignados a pedidos existentes
-					if !obtener_prods?(eleg[0],eleg[1],pedido)
-						## no se deberia llegar aqui nunca.
-						return false
-					end
-				else
-					# No hay forma de manejar pedidos >200 x almacen.
-					return false
-				end
+			##obtenemos lo usado por pedidos anteriores
+			pedidos=Pedido.first(Pedido.count-1)[0..Pedido.count-2]
+			pedidos.each do |pedidox|
+			if pedidox.sku.equal? pedido.sku
+				usado+=pedidox.cantidad
 			end
+			end
+
+		##si la cantidad es menor a la diferencia entre el total y lo usado, se acepta.
+		if pedido.cantidad<=(total-usado)
+			true
 		else
-			##si no hay, retornamos false
-			return false
+			false
 		end
 	end
+
+				##VERSION 1.0
+				##tenemos que ver cuanto realmente tenemos disponible
+				##usado= get_cantidad_usada(almacen.id,pedido)
+				##disponible=resultado-usado
+				##ver si necesitamos mas o menos de lo que tenemos disponible
+				##si lo disponible es mayor que 0
+				##==============
+
+				##if disponible>0
+
+	#			if contador+disponible<cant
+					##si el pedido nos exige mas de lo que tenemos disponible
+	#				contador+=disponible
+					##hacemos append de el id del almacen y de la cantidad de prod. que usaremos en 'elegido'
+	#				elegido.append [almacen.almacen_id,disponible]
+					#si es menor, hacemos append de el id y la diferencia entre el contal y lo que llevamos
+	#			else
+					#elegido.append [almacen.almacen_id,cant-contador]
+					##en teoria ya podemos satisfacer el pedido.
+	#				hay=true
+	#				break
+	#			end
+
+
+	#	if hay
+			#hay que obtener los id's de los productos que vamos a usar.
+			#necesitamos llamar a metodo getstock con el sku y el almacen id
+	#		elegido.each do |eleg|
+				#La condicion es pq si es >200, entonces tenemos problemas con el display de productos.
+				# (tenemos limit=200)
+	#			if eleg[1]<200
+				#obtener prods retorna true si hay suficientes prods en bodega que no esten asignados a pedidos existentes
+	#				if !obtener_prods?(eleg[0],eleg[1],pedido)
+						## no se deberia llegar aqui nunca.
+	#					return false
+	#				end
+	#			else
+					# No hay forma de manejar pedidos >200 x almacen.
+	#				return false
+	#			end
+	#		end
+	#	else
+			##si no hay, retornamos false
+		#	return false
+	#	end
+	#end
 
 
 
