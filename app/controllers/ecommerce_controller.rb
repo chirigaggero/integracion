846642ac0ab3901b36e879ee28212a376ae4aca3
Christian Gaggero <chirigaggero@gmail.com>
2015-06-29 18:23:09 -0300

    cambios ecommerce

--- app/controllers/ecommerce_controller.rb
+++ app/controllers/ecommerce_controller.rb
@@ -1,25 +1,64 @@
 class EcommerceController < ApplicationController
 
   def products
+    message = ""
   	if params[:product] and params[:quantity]
   		if params[:product]=="azucar"
-  			session[:azucar]=params[:quantity]
+        sku = ProductInfo.find_by(product_id: "azucar").sku
+        if params[:quantity].to_i == 0
+          message = "Debes ingresar un numero entero mayor a cero."
+        elsif Bodega.cantidad_total_sku(sku) < params[:quantity].to_i
+          message = "No hay stock suficiente de este producto"
+        else
+          session[:azucar]=params[:quantity]
+        end
   		end
   		if params[:product]=="madera"
-  			session[:madera]=params[:quantity]
+        sku = ProductInfo.find_by(product_id: "madera").sku
+        if params[:quantity].to_i == 0
+          message = "Debes ingresar un numero entero mayor a cero."
+        elsif Bodega.cantidad_total_sku(sku) < params[:quantity].to_i
+          message = "No hay stock suficiente de este producto"
+        else
+          session[:madera]=params[:quantity]
+        end
   		end
   		if params[:product]=="celulosa"
-  			session[:celulosa]=params[:quantity]
+        sku = ProductInfo.find_by(product_id: "celulosa").sku
+        if params[:quantity].to_i == 0
+          message = "Debes ingresar un numero entero mayor a cero."
+        elsif Bodega.cantidad_total_sku(sku) < params[:quantity].to_i
+          message = "No hay stock suficiente de este producto"
+        else
+          session[:celulosa]=params[:quantity]
+        end
   		end
   		if params[:product]=="chocolate"
-  			session[:chocolate]=params[:quantity]
+        sku = ProductInfo.find_by(product_id: "chocolate").sku
+        if params[:quantity].to_i == 0
+          message = "Debes ingresar un numero entero mayor a cero."
+        elsif Bodega.cantidad_total_sku(sku) < params[:quantity].to_i
+          message = "No hay stock suficiente de este producto"
+        else
+          session[:chocolate]=params[:quantity]
+        end
   		end
   		if params[:product]=="pastadesemola"
-  			session[:pastadesemola]=params[:quantity]
+        sku = ProductInfo.find_by(product_id: "pastadesemola").sku
+        if params[:quantity].to_i == 0
+          message = "Debes ingresar un numero entero mayor a cero."
+        elsif Bodega.cantidad_total_sku(sku) < params[:quantity].to_i
+          message = "No hay stock suficiente de este producto"
+        else
+          session[:pastadesemola]=params[:quantity]
+        end
   		end
-
-  		flash[:notice] = "Se ha agragado exitosamente al carrito de compras. (producto: "+params[:product] + ", cantidad:"+params[:quantity]+")"
-  	end
+      if message != ""
+        flash[:notice] = message
+      else
+  		  flash[:notice] = "Se ha agragado exitosamente al carrito de compras. (producto: "+params[:product] + ", cantidad:"+params[:quantity]+")"
+  	  end
+    end
   end
 
   def shoppingcart
@@ -33,5 +72,31 @@ class EcommerceController < ApplicationController
   end
 
   def checkout
+    # calculamos el total
+    total = 0
+    if session[:azucar]
+      total += session[:azucar]
+    end
+    if session[:madera]
+      total += session[:madera]
+    end
+    if session[:celulosa]
+      total += session[:celulosa]
+    end
+    if session[:chocolate]
+      total += session[:chocolate]
+    end
+    if session[:pastadesemola]
+      total += session[:pastadesemola]
+    end
+    # generamos boleta
+    url = "http://integra3.ing.puc.cl/b2b/get_token/"
+    headers = {"Content-Type"=> "application/json", "Accept" => "application/json"}
+    body = {"proveedor" => "grupo8", "cliente" => "CLIENTE", "total" => "TOTAL"}
+    result = HTTParty.put(url, :headers => headers, :body => body.to_json)
+
+
+    url = "http://chiri.ing.puc.cl/banco/pagoenlinea?callbackUrl=URL_OK&cancelUrl=URL_FAIL&boletaId=ID"
+
   end
 end
